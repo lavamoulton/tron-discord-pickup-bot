958b8bf4332313f1996b61b36f03956b0184664f
Hodor <danielzmoulton@gmail.com>
2020-07-09 15:38:19 -0400

    Clean up and comments

--- index.js
+++ index.js
@@ -30,9 +30,14 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+
+  // skip message if not in the right channel
+
   if (msg.channel.name !== 'pickup') {
     return;
   }
+
+  // !add functionality
 
   if (msg.content.toLowerCase() === '!add tst') {
     addPlayer(tstList, msg);
@@ -47,23 +52,29 @@ client.on('message', msg => {
     return;
   }
   if (msg.content.toLowerCase() === '!add sumo') {
-    if (addPlayer(tstList, msg)) {
-      addPlayer(wstList, msg);
-    }
-  }
-  if (msg.content.toLowerCase() === '!add nowst') {
-    if (addPlayer(fortList, msg)) {
+    if (addPlayer(wstList, msg)) {
       addPlayer(tstList, msg);
     }
+    return;
+  }
+  if (msg.content.toLowerCase() === '!add nowst') {
+    if (addPlayer(tstList, msg)) {
+      addPlayer(fortList, msg);
+    }
+    return;
   }
   if (msg.content.toLowerCase() === '!add') {
-    if (addPlayer(fortList, msg)) {
+    if (addPlayer(wstList, msg)) {
       if (addPlayer(tstList, msg)) {
-        addPlayer(wstList, msg);
+        addPlayer(fortList, msg);
       }
     }
     return;
   }
+
+  // Utility functionality
+  
+  // #TODO: pull out into separate function, passing list argument in 
   if (msg.content.toLowerCase() === '!who') {
     if (tstList.values.length === 0 && fortList.values.length === 0 && wstList.values.length === 0) {
       msg.channel.send('Nobody is added yet');
@@ -78,11 +89,18 @@ client.on('message', msg => {
         `Fort players added (${fortList.values.length}/${fortList.options.maxPlayers}): ${fortList.values.map(player => player.name)}`
       );
     if (wstList.values.length > 0)
-        msg.channel.send(
-          `WST players added (${wstList.values.length}/${wstList.options.maxPlayers}): ${wstList.values.map(player => player.name)}`
-        );
+      msg.channel.send(
+        `WST players added (${wstList.values.length}/${wstList.options.maxPlayers}): ${wstList.values.map(player => player.name)}`
+      );
     return;
   }
+  if (msg.content.toLowerCase() === '!help') {
+    msg.channel.send(printHelpMessage(msg))
+    return;
+  }
+
+  // !remove functionality
+
   if (msg.content.toLowerCase() === '!remove tst') {
     removePlayer(tstList, msg);
     return;
@@ -116,73 +134,9 @@ client.on('message', msg => {
     }
     return;
   }
-  if (msg.content.toLowerCase() === '!help') {
-    msg.channel.send(printHelpMessage(msg))
-  }
 });
 
-function printHelpMessage(msg) {
-  const playerName = msg.author.username;
-  if (msg.member.nickname) {
-     playerName = msg.member.nickname;
-  }
-  return (`<@${msg.author.id}>, available pickup commands are:\n` +
-            `**!add**: Add to all available game modes\n` +
-            `**!add <gamemode>**: Add to a specific game mode (fort, tst, or wst)\n` +
-            `**!add nowst**: Add to fort and tst game modes only\n` +
-            `**!add sumo**: Add to sumo game modes only\n\n` +
-            `**!remove**: Remove from all added game modes\n` +
-            `**!remove <gamemode>**: Remove from a specific game mode (fort, tst, or wst)\n\n` +
-            `**!who**: Display who is added to each game mode\n`
-  );
-}
-
-function clearOtherLists(list, msg) {
-  if (list.options.name === 'TST') {
-    clearDuplicates(list, wstList, msg);
-    clearDuplicates(list, fortList, msg);
-  }
-  if (list.options.name === 'Fort') {
-    clearDuplicates(list, wstList, msg);
-    clearDuplicates(list, tstList, msg);
-  }
-  if (list.options.name === 'WST') {
-    clearDuplicates(list, fortList, msg);
-    clearDuplicates(list, tstList, msg);
-  }
-}
-
-function clearDuplicates(startedList, targetList, msg) {
-  startedList.values.forEach(player => removePlayerId(targetList, player.id));
-  printList(targetList, msg.channel);
-}
-
-function removePlayerId(list, id) {
-  for (let index in list.values) {
-    if (list.values[index].id === id) {
-      list.values.splice(index, 1);
-      return true;
-    }
-  }
-  return false;
-}
-
-function printList(list, channel) {
-  if (list.values.length === 0) {
-    channel.send(`${list.options.name} list is empty!`);
-  } else {
-    const newList = list.values.map(player => `<@${player.id}>`);
-    channel.send(`${list.options.name} list updated (${list.values.length}/${list.options.maxPlayers}: ${newList}`);
-  }
-}
-
-function removePlayer(list, msg) {
-  if (removePlayerId(list, msg.author.id)) {
-    printList(list, msg.channel);
-    return;
-  }
-  msg.reply(`You are not on the ${list.options.name} list`);
-}
+// Add helper functions
 
 function addPlayer(list, msg) {
   /**
@@ -217,6 +171,71 @@ function addPlayer(list, msg) {
   msg.channel.send(`${list.options.name} list updated (${list.values.length}/${list.options.maxPlayers}): ${newList}`);
   return true;
 }
+
+// Utility helper functions
+
+function printHelpMessage(msg) {
+  return (`<@${msg.author.id}>, available pickup commands are:\n` +
+            `**!add**: Add to all available game modes\n` +
+            `**!add <gamemode>**: Add to a specific game mode (fort, tst, or wst)\n` +
+            `**!add nowst**: Add to fort and tst game modes only\n` +
+            `**!add sumo**: Add to sumo game modes only\n\n` +
+            `**!remove**: Remove from all added game modes\n` +
+            `**!remove <gamemode>**: Remove from a specific game mode (fort, tst, or wst)\n\n` +
+            `**!who**: Display who is added to each game mode\n`
+  );
+}
+
+function printList(list, channel) {
+  if (list.values.length === 0) {
+    channel.send(`${list.options.name} list is empty!`);
+  } else {
+    const newList = list.values.map(player => `<@${player.id}>`);
+    channel.send(`${list.options.name} list updated (${list.values.length}/${list.options.maxPlayers}: ${newList}`);
+  }
+}
+
+// Remove helper functions
+
+function clearOtherLists(list, msg) {
+  if (list.options.name === 'TST') {
+    clearDuplicates(list, wstList, msg);
+    clearDuplicates(list, fortList, msg);
+  }
+  if (list.options.name === 'Fort') {
+    clearDuplicates(list, wstList, msg);
+    clearDuplicates(list, tstList, msg);
+  }
+  if (list.options.name === 'WST') {
+    clearDuplicates(list, fortList, msg);
+    clearDuplicates(list, tstList, msg);
+  }
+}
+
+function clearDuplicates(startedList, targetList, msg) {
+  startedList.values.forEach(player => removePlayerId(targetList, player.id));
+  printList(targetList, msg.channel);
+}
+
+function removePlayerId(list, id) {
+  for (let index in list.values) {
+    if (list.values[index].id === id) {
+      list.values.splice(index, 1);
+      return true;
+    }
+  }
+  return false;
+}
+
+function removePlayer(list, msg) {
+  if (removePlayerId(list, msg.author.id)) {
+    printList(list, msg.channel);
+    return;
+  }
+  msg.reply(`You are not on the ${list.options.name} list`);
+}
+
+// Randomize ready to start game modes
 
 function getRandom(list) {
   if (list.options.name === 'TST') {
